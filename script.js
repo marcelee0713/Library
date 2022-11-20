8dddb5a76f1d5c2bfd03e6a7adda5c8a6840e660
Marcelee Laylay <youmustbeajoke@gmail.com>
2022-11-20 10:21:32 +0800

    Finished the UI

---
+++ script.js
@@ -0,0 +1,20 @@
+let books = [
+    {
+        title: "Elden Ring",
+        author: "FromSoftware",
+        pages: "192",
+        read: false
+    },
+    {
+        title:  "Deez Nuts",
+        author: "AdmiralBulldog",
+        pages: "100",
+        read: false
+    },
+    {
+        title: "Defense of the Acients",
+        author: "Ice Frog",
+        pages: "200",
+        read: true
+    }
+]

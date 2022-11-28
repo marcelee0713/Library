c62e1fb5c4b09d3e8cacd142ee2388eab8688b23
Marcelee Laylay <youmustbeajoke@gmail.com>
2022-11-28 13:41:50 +0800

    Made a new branch and the plain constructors by using a class

--- script.js
+++ script.js
@@ -69,11 +69,13 @@ let books = [
     }
 ];
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book{
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary(){
